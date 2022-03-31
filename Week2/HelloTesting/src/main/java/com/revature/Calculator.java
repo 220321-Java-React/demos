93bcfea79a24ac5f36421ae3fd13cb05331de08d
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-03-31 16:34:27 -0400

    Started HelloTesting

---
+++ Week2/HelloTesting/src/main/java/com/revature/Calculator.java
@@ -0,0 +1,27 @@
+package com.revature;
+
+/**
+ * @author BenjaminPetruzziello
+ *
+ */
+public class Calculator {
+
+	//we're going to make 3 simple methods that perform simple math calculations
+	//remember, unit tests tend to test the smallest possible blocks of code (methods usually)
+	
+	//addition
+	public int add(int x, int y) {
+		return x + y;
+	}
+	
+	//subtraction
+	public int subtract(int x, int y) {
+		return x - y;
+	}
+	
+	//division
+	public int divide(int x, int y) {
+		return x / y;
+	}
+	
+}
---
+++ Week2/HelloTesting/src/test/java/com/revature/CalculatorTests.java
@@ -0,0 +1,31 @@
+package com.revature;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+//This is a test class! It'll be full of methods that test our Calculator Class
+public class CalculatorTests {
+
+	//initialize a Calculator object so we can use and test it's methods
+	Calculator calc = new Calculator();
+	
+	//ben may or may not show @Before/AfterAll
+	
+	
+	//Unit tests below-------------------------
+	
+	//TO RUN A TEST CLASS: right click the test class -> run as -> JUnit test
+	
+	@Test
+	public void testAdd() {
+		
+		System.out.println("TESTING ADD METHOD");
+	
+		int result = calc.add(5, 5); //we're assigning the result of the add() method to this result variable
+		
+		assertTrue(result == 10); //this test will pass if result == 10.
+		
+	}
+	
+}
