3bfcd6471112cdef25747a4e5e583473b4e58406
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-04-07 15:15:35 -0400

    First ugly table

--- Week3/Week3SQL/ddl_datatypes.sql
+++ Week3/Week3SQL/ddl_datatypes.sql
@@ -10,4 +10,35 @@ CREATE TABLE users(
 	username TEXT --text is my text datatype of choice 
 );
 
- --we created a (bad) table of users that simply tracks a username
+--we created a (bad) table of users that simply tracks a username
+
+--we can view this new table (and any other new tables by right clicking the schema -> view diagram -> 
+--right click your schema -> refresh)
+
+--OH NO! I meant to add an age column to my table. I can use the ALTER command to change or add columns
+ALTER TABLE users ADD user_age int;
+
+--We can also rename columns using alter
+ALTER TABLE users RENAME COLUMN user_age TO users_age;
+
+--we can DROP a table with the drop command
+DROP TABLE users;
+
+--not shown: TRUNCATE, which drops all records in a table
+--common after backups, if you want to save the 
+
+--TRUNCATE TABLE users;
+
+--so DROP deletes a table/column (and all the data), TRUNCATE only deletes the data.
+
+--This is an extremely rudimentary table, just to show DDL commands and their syntax
+
+---------------------------------------------------------------------------------------
+
+--SQL Datatypes
+
+
+
+
+
+
