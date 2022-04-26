0830d923e8a009984597bb17fbbe23185e21a009
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-04-26 16:19:15 -0400

    first fetch request done

--- P1Demo/FrontEnd/krustyKrab.js
+++ P1Demo/FrontEnd/krustyKrab.js
@@ -22,12 +22,45 @@ async function getEmployees() {
     if(response.status === 200) { //"if the response has a status code of 200..."
 
         //parse the JSON we get back into a JS object
+        //.json() is the JS method that takes json and turns it into a JS object
+        let data = await response.json();
 
         //log the actual employee data that's been parsed from JSON (good for debugging)
+        console.log(data);
 
-        //For every Employee object we get back, put it in the table
+        //For every Employee object we get back from our fetch request, put it in the table
+        for(let employee of data){
 
+            //create a table row
+            let row = document.createElement("tr");
 
+            //create a data cell for each employee field
+            let cell = document.createElement("td");
+            //fill the cell with the appropriate employee data
+            cell.innerHTML = employee.employee_id;
+            //add the td element (data cell) to the tr element (table row)
+            row.appendChild(cell);
+
+            //we do this^^^^ for every column in employees
+
+            let cell2 = document.createElement("td");
+            cell2.innerHTML = employee.first_name;
+            row.appendChild(cell2);
+
+            let cell3 = document.createElement("td");
+            cell3.innerHTML = employee.last_name;
+            row.appendChild(cell3);
+
+            let cell4 = document.createElement("td");
+            cell4.innerHTML = employee.role.role_title;
+            row.appendChild(cell4);
+
+            //append the tr (which we called "row") to the table body (tbody)
+            //a new row will be appended FOR every employee that got returned in the fetch()
+            document.getElementById("employeeBody").appendChild(row);
+
+        }
+        //so here, for every employee object, we create a new row, fill it with data, add it to table
 
     } else {
         //alert causes a popup!!
