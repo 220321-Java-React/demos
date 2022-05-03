dd33427d5c545faddc59820e6056214bd9a0f3a2
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-05-03 14:09:22 -0400

    Started hello-react

--- Week7/hello-react/src/App.tsx
+++ Week7/hello-react/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react'; //we will always need to do this in most logic files
 
 //The App.tsx file is where we put what we want displayed on the webpage
+//You can add standard TSX, or entire components (which will have their own TSX)
+
 //the return() funtion is where we put what we want to render
 function App() {
+  //the return() function takes TSX (Typscript XML) and asks like HTML. creates our view!
   return (
     <div className="App">
       <h1>Hello React</h1>
+      <p>We are writing in TSX, which is like HTML for React </p>
     </div>
   );
 }
