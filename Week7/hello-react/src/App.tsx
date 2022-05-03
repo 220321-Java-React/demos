949bb7833b3979000981f49c86a63adbec0ae27f
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-05-03 14:55:33 -0400

    Created class component

--- Week7/hello-react/src/App.tsx
+++ Week7/hello-react/src/App.tsx
@@ -6,6 +6,7 @@ import React from 'react'; //we will always need to do this in most logic files
 //the return() funtion is where we put what we want to render
 function App() {
   //the return() function takes TSX (Typscript XML) and asks like HTML. creates our view!
+  //this acts much like the HTML body
   return (
     <div className="App">
       <h1>Hello React</h1>
@@ -14,4 +15,5 @@ function App() {
   );
 }
 
+//This lets us export this App.tsx to potentially be used in other applications. Which we won't do
 export default App;
---
+++ Week7/hello-react/src/components/FirstClassComponent/FirstClassComponent.tsx
@@ -0,0 +1,24 @@
+//the first step in any component is to import React from "react";
+import React from "react";
+
+//Below, we're creating a CLASS BASED COMPONENT with Typescript. The component takes two parameters
+//The parameters are props and state, which we'll mess with later
+export class FirstClassComponent extends React.Component<{}, {}>{
+
+    //creating a string variable, which we'll databind below
+    //in a TS CLASS, we don't use let/var/const. They aren't recognized as keywords
+    words:string = "I am stored in a variable in the class component";
+
+    //Every Class Based Component must contain the render() function
+    //This is what the component will actually display (hence render) onto the webpage
+    render() {
+        //the render function must contain a return statement, where the TSX is actually held
+        return(
+            <div>
+                <h3>Hello from our first class-based component!</h3>
+                <p>{this.words}</p>
+            </div>
+        )
+    }
+
+}
