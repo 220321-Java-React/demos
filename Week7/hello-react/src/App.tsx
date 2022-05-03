1a40f58ac686199ec4376ff1a495a38b720b83ff
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-05-03 12:01:23 -0400

    Started hello-react

---
+++ Week7/hello-react/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'; //we will always need to do this in most logic files
+
+//The App.tsx file is where we put what we want displayed on the webpage
+//the return() funtion is where we put what we want to render
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+    </div>
+  );
+}
+
+export default App;
