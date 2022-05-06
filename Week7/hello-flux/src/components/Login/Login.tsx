3ea122aa8341ea78089dcba8cacb5532cb5c0fe8
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-05-06 16:16:38 -0400

    started login component

---
+++ Week7/hello-flux/src/components/Login/Login.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+export const Login: React.FC<any> = () => {
+
+
+
+    //BEN NEEDS TO ADD A LOT OF JUNK INCLUDING ONCLICK, 
+    return(
+        <div className="login">
+
+            <div className="text-container">
+                <h1>Welcome to PokeMart!</h1>
+                <h3>Sign in to browse for a pokemanz</h3>
+            </div>
+
+            <div className="inputs-container">
+                <div className="login-container">
+
+                </div>
+            </div>
+
+        <button className="login-button" ></button>
+
+        </div>
+    )
+
+} 
