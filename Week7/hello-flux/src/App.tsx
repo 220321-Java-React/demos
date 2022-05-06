b090a4832c9eb54164d9debce3a214424da0e9b6
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-05-06 16:21:58 -0400

    rendered a janky login component

--- Week7/hello-flux/src/App.tsx
+++ Week7/hello-flux/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
+import { Login } from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
-     
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
