9b5d7e8022181ec97d3d1ad57665d680801399c7
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-05-06 12:53:59 -0400

    styled posts

--- Week7/hello-events-routing/src/components/PostComponent/PostComponent.tsx
+++ Week7/hello-events-routing/src/components/PostComponent/PostComponent.tsx
@@ -5,7 +5,6 @@ import "./PostComponent.css"
 //post is the props that we're getting from PostContainer
 export const PostComponent: React.FC<any> = (post:any) => {
 
-
     return(
         <div className="post-container">
             <div className="post-profile">
