858b8a1ed0755f872ed517b185c2bfa57350ea63
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-05-06 10:50:56 -0400

    started PostCOntainer

---
+++ Week7/hello-events-routing/src/components/PostContainerComponent/PostContainer.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react"
+import { data } from "../../data"
+
+import { PostComponent } from "../PostComponent/PostComponent"
+
+export const PostContainer: React.FC<any> = (data) => {
+
+    //useState lets us declare a state variable, a mutator (like a setter), and a default value 
+    //remember we need state to send data to other components
+    let [posts, setPosts] = useState([]) //empty array because our post data is an array (data.ts)
+
+    //useEffect will execute some code, either when something changes, 
+    //OR for every piece of data in a given list
+    //so our posts state variable will be filled with the Array from data.ts
+    useEffect(() => {
+        setPosts(data.data)
+    }, [posts]) 
+
+    return(
+        //for every post in our data array, render a Post component for it
+        <div>
+
+        </div>
+    )
+
+}
