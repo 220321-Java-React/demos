80f287486a9663904b4bda339e60a965c002c3ab
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-05-09 22:19:34 -0400

    rendered and styled login component

--- Week7/hello-flux/src/components/Login/Login.tsx
+++ Week7/hello-flux/src/components/Login/Login.tsx
@@ -1,26 +1,32 @@
 import React from "react"
 
+import "./Login.css"
+
 export const Login: React.FC<any> = () => {
 
 
 
-    //BEN NEEDS TO ADD A LOT OF JUNK INCLUDING ONCLICK, 
+    //BEN NEEDS TO ADD A LOT OF JUNK INCLUDING ONCLICK AND ONCHANGE 
     return(
         <div className="login">
 
             <div className="text-container">
                 <h1>Welcome to PokeMart!</h1>
                 <h3>Sign in to browse for a pokemanz</h3>
-            </div>
 
-            <div className="inputs-container">
-                <div className="login-container">
-
+                <div className="input-container">
+                    <input type="text" name="username" placeholder="username"/>
                 </div>
+                <div className="input-container">
+                    <input type="password" name="password" placeholder="password"/>
+                </div>
+
+                <button className="login-button">Catch 'em all!</button>
             </div>
 
-        <button className="login-button" >Catch 'em all!</button>
-
+            <div className="disclaimer">
+                <p>PokeMart© and its affiliates do not condone the misuse or mistreatment of pokemon</p>
+            </div>
         </div>
     )
 
