a92f7637d9800bc8bd50de6141cb615eb4f7a7d3
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-05-11 12:37:47 -0400

    finished PokeReducer

--- Week7/hello-flux/src/reducers/PokeReducer.ts
+++ Week7/hello-flux/src/reducers/PokeReducer.ts
@@ -1,17 +1,24 @@
+import { GET_POKE } from "../actions/actionTypes";
 import { IPoke } from "../store/types";
 
 //remember, reducers help us take in new data from actions, 
 //which then gets calculated and sent to the store
-const initialState:IPoke = {
+let initialState:IPoke = {
     id: 0,
     name:"",
     sprite:null
 }
 
-type Action = {type: string, payload: object}
+type Action = {type: string, payload: any}
 
 export const PokeReducer = (state:IPoke = initialState, action:Action) => {
     switch(action.type){
+
+        case GET_POKE:
+            initialState = action.payload
+            return{
+                ...initialState
+            }
         default:
             return state
     }
