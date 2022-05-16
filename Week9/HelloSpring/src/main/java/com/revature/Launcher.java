6e90a059fe4660d76182f7ece7c697baeb78c94f
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-05-16 11:02:59 -0400

    applicationContext.xml the good ol fashioned way

--- Week9/HelloSpring/src/main/java/com/revature/Launcher.java
+++ Week9/HelloSpring/src/main/java/com/revature/Launcher.java
@@ -3,6 +3,8 @@ package com.revature;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.revature.models.User;
+
 public class Launcher {
 
 	public static void main(String[] args) {
@@ -11,6 +13,13 @@ public class Launcher {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
 		//we are instantiating a new ApplicationContext to hold our spring beans
 		//BUT in order to configure it we need to give it the class path context to our config file (our XML file)
+		
+		
+		//Now that we have a Spring Container^^^ which is storing User and Account beans, we can make some!
+		
+		//we're calling our ApplicationContext and attempting to get a Bean with the name = "user"
+		//a cast is needed because the ApplicationContext can't guess what kind of object this will be
+		User u = (User)ac.getBean("user");
 		
 		
 		
